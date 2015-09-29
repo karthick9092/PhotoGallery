27fbec67e6aae0c5d1b41d0f770dc2bab60b72a0
karthick9092 <karthickmail.u@gmail.com>
2015-09-30 01:36:27 +0530

    Sample Template Implemented
    Signed-off-by: karthick9092 <karthickmail.u@gmail.com>

--- PhotoGallery/src/main/webapp/js/controller/galleryController.js
+++ PhotoGallery/src/main/webapp/js/controller/galleryController.js
@@ -1,12 +1,33 @@
 appModule.controller('galleryController', function ($scope, Lightbox) {
 	
 	$scope.images = [ {
-		'url' : 'http://localhost:8080/PhotoGallery/images/images-1.jpg',
-		'thumbUrl' : 'http://localhost:8080/PhotoGallery/images/images-1.jpg',
+		'url' : 'images/marriage/img1.jpg',
+		'thumbUrl' : 'images/marriage/img1.jpg',
 	}, {
-		'url' : 'http://localhost:8080/PhotoGallery/images/images-2.jpg',
-		'thumbUrl' : 'http://localhost:8080/PhotoGallery/images/images-2.jpg',
-	} ];
+		'url' : 'images/marriage/img2.jpg',
+		'thumbUrl' : 'images/marriage/img2.jpg',
+	}, {
+		'url' : 'images/marriage/img3.jpg',
+		'thumbUrl' : 'images/marriage/img3.jpg',
+	}, {
+		'url' : 'images/marriage/img4.jpg',
+		'thumbUrl' : 'images/marriage/img4.jpg',
+	}, {
+		'url' : 'images/marriage/img5.jpg',
+		'thumbUrl' : 'images/marriage/img5.jpg',
+	}, {
+		'url' : 'images/marriage/img6.jpg',
+		'thumbUrl' : 'images/marriage/img6.jpg',
+	}, {
+		'url' : 'images/marriage/img7.jpg',
+		'thumbUrl' : 'images/marriage/img7.jpg',
+	}, {
+		'url' : 'images/marriage/img8.jpg',
+		'thumbUrl' : 'images/marriage/img8.jpg',
+	}, {
+		'url' : 'images/marriage/img9.jpg',
+		'thumbUrl' : 'images/marriage/img9.jpg',
+	}];
 
 	$scope.openLightboxModal = function(index) {
 		Lightbox.openModal($scope.images, index);
